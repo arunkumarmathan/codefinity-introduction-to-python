d7f9b29d1d690dc0d79d9cfe8a30bc145ef3ef1c
arunkumarmathan <>
2026-01-23 02:13:33 +0000

    Challenge: Tuple Operations

---
+++ other_data_types/challenge_tuple_operations/main.py
@@ -0,0 +1,20 @@
+# Current inventory on shelf
+shelf = ("apples", "oranges", "bananas", "apples", "grapes", "bananas", "apples")
+
+# TODO: Count apples and print
+print("Number of Apples:", apple_count)
+
+# TODO: Find first banana index and print
+print("First Banana Index:", banana_index)
+
+# TODO: Print apple stock status
+print("Apples need to be restocked.")
+print("Apples are sufficiently stocked.")
+
+# TODO: Print grape stock status
+print("Grapes need to be restocked.")
+print("Grapes are sufficiently stocked.")
+
+# TODO: Print oranges index or out of stock message
+print("Oranges are at index:", orange_index)
+print("Oranges are out of stock.")
